68abf6886a2b875bcad290bda759f9b7b6f5b4cf
Roman Kavinskyi <mrronanx@gmail.com>
2021-07-31 16:24:29 +0700

    Initial Commit

---
+++ Test Task. List Swipe Actions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test Task. List Swipe Actions
+//
+//  Created by Roman Kavinskyi on 7/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test Task. List Swipe Actions/Test_Task__List_Swipe_ActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Task__List_Swipe_ActionsApp.swift
+//  Test Task. List Swipe Actions
+//
+//  Created by Roman Kavinskyi on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_Task__List_Swipe_ActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
